eb3581e264aeea8dbf2f658c28c8d5a373df7633
Duc Anh <ducanh.le1908@gmail.com>
2022-07-28 17:26:07 +0700

    update database and search

--- model/search.js
+++ model/search.js
@@ -14,8 +14,8 @@ class Search {
 
 
 seachByCateGory() {
-  let query = `select TenSach,TenTheLoai,MoTa,TinhTrang from Sach join theloai  on sach.theloai_id = theloai.id
-  join tinhtrangsach on sach.tinhtrang_id = tinhtrangsach.id`;
+  let query = `select TenSach,TenTheLoai,MoTa,TinhTrang from Sach join theloai  on Sach.TheLoai_id = TheLoai.id
+  join TinhTrangSach on Sach.TinhTrang_id = TinhTrangSach.id`;
   this.connection.query(query,(err,data)=> {
     if(err) {
       throw err.stack
@@ -27,12 +27,12 @@ seachByCateGory() {
 
 getBookshelfsCategory(name) {
   return new Promise((resolve, reject) => {
-    this.connection.query(`select TenSach,Sach.MoTa as 'SachMoTa',TenTheLoai,TinhTrang,TenKho,Kho.Mota as 'KhoMoTa',SLDeSach,SLSachTrongKho from Sach join theloai on sach.theloai_id = theloai.id
-    join tinhtrangsach on sach.tinhtrang_id = tinhtrangsach.id
-    join kho on sach.kho_id = kho.id
-     where 		tentheloai like '%${name.search}%'
-			or tinhtrang like '%${name.search}%'
-            or tenkho like '%${name.search}%'
+    this.connection.query(`select TenSach,Sach.MoTa as 'SachMoTa',TenTheLoai,TinhTrang,TenKho,Kho.Mota as 'KhoMoTa',SLDeSach,SLSachTrongKho from Sach join TheLoai on Sach.TheLoai_id = TheLoai.id
+    join TinhTrangSach on Sach.TinhTrang_id = TinhTrangSach.id
+    join Kho on Sach.Kho_id = Kho.id
+     where 		TenTheLoai like '%${name.search}%'
+			or TinhTrang like '%${name.search}%'
+            or TenKho like '%${name.search}%'
             or TenSach like '%${name.search}%'
             `, (err, data) => {
       if (err) {
@@ -45,9 +45,9 @@ getBookshelfsCategory(name) {
 }
 setBooksCategory() {
   return new Promise((resolve, reject) => {
-    let query = `select TenSach,Sach.MoTa as 'SachMoTa',TenTheLoai,TinhTrang,TenKho,Kho.Mota as 'KhoMoTa',SLDeSach,SLSachTrongKho from Sach join theloai on sach.theloai_id = theloai.id
-    join tinhtrangsach on sach.tinhtrang_id = tinhtrangsach.id
-    join kho on sach.kho_id = kho.id`
+    let query = `select TenSach,Sach.MoTa as 'SachMoTa',TenTheLoai,TinhTrang,TenKho,Kho.Mota as 'KhoMoTa',SLDeSach,SLSachTrongKho from Sach join TheLoai on Sach.TheLoai_id = TheLoai.id
+    join TinhTrangSach on Sach.TinhTrang_id = TinhTrangSach.id
+    join Kho on Sach.Kho_id = Kho.id`
   this.connection.query(query,(err,data)=> {
     if(err) {
       reject(err)
